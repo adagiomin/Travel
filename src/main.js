d67fe112418d9c4e157f295fa6b6cd4da2ed5352
liuminhere <liumin@hdu.edu.cn>
2020-04-02 17:33:18 +0800

    首页Header

--- src/main.js
+++ src/main.js
@@ -4,7 +4,13 @@ import Vue from 'vue'
 import App from './App'
 import router from './router'
 
+import 'styles/reset.css'   //styles是别名，同@符号一样
+import 'styles/border.css'
+import 'styles/iconfont.css'
+import fastClick from 'fastclick'
+
 Vue.config.productionTip = false
+fastClick.attach(document.body)
 
 /* eslint-disable no-new */
 new Vue({
--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,6 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import Home from '@/components/Home'
+import Home from '@/pages/home/Home'  //@符值src目录
 
 Vue.use(Router)
 
