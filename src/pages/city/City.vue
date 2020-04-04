15682a84339be0efd7bcf3d5ee6213ee44f79a87
liuminhere <liumin@hdu.edu.cn>
2020-04-04 19:47:28 +0800

    city-components

--- src/pages/city/City.vue
+++ src/pages/city/City.vue
@@ -2,8 +2,8 @@
 <div>
     <city-header></city-header>
     <city-search></city-search>
-    <city-list :cities="cities" :hot="hotCities"></city-list>
-    <city-alphabet :cities="cities"></city-alphabet>
+    <city-list :cities="cities" :hot="hotCities" :letter="letter"></city-list>
+    <city-alphabet :cities="cities" @change="handleLetterChange"></city-alphabet>
 </div>
 </template>
 
@@ -26,6 +26,7 @@ export default {
         return {
             cities:{},//城市列表按照A-Z为键，值为相应城市
             hotCities:[],//热门城市
+            letter:'',//当前字母
         }
     },
     mounted(){
@@ -42,6 +43,9 @@ export default {
                 this.cities = data.cities
                 this.hotCities = data.hotCities 
             }
+        },
+        handleLetterChange(letter){
+            this.letter = letter
         }
     }
 }
--- src/pages/city/components/Alphabet.vue
+++ src/pages/city/components/Alphabet.vue
@@ -1,6 +1,13 @@
 <template>
   <ul class="list">
-      <li class="item" v-for="(item,key) of cities" :key="key">{{key}}</li>
+      <li class="item" 
+      v-for="item of letters" 
+      :key="item"
+      :ref="item"
+      @click="handleLetterClick"
+      @touchstart="handleTouchStart"
+      @touchmove="handleTouchMove"
+      @touchend="handleTouchEnd">{{item}}</li>
   </ul>
 </template>
 
@@ -9,6 +16,59 @@ export default {
     name:'CityAlphabet',
     props:{
         cities:Object,
+    },
+    data(){
+        return {
+            touchStatus:false,
+            startY: 0,
+            timer: null
+        }
+    },
+    updated(){
+        //当页面的数据被更新的时候，同时页面完成了自己的渲染后,这个钩子函数被执行
+        // 代码优化，不再将this.$refs['A'][0].offsetTop放在handleTouchMove
+        // 不然手指每次移动，都要执行一次
+        this.startY = this.$refs['A'][0].offsetTop
+    },
+    computed:{
+        letters(){
+            const letters = []
+            for(let i in this.cities){
+                letters.push(i)
+            }
+            return letters
+        }
+    },
+    methods:{
+        handleLetterClick(e){
+            this.$emit('change',e.target.innerText)
+        },
+        handleTouchStart(){
+            this.touchStatus = true
+        },
+        handleTouchMove(e){
+            if(this.touchStatus){
+                if(this.timer){
+                    clearTimeout(this.timer)
+                }
+                // 使用timer来截流，大大节约handleTouchMove的执行频率，提高网站效率
+                // 如果正在做这件事情的时候，给它延迟16毫秒的事件再执行
+                // 假如在这16毫秒时间内你又操作了，它会将上次的操作清除，重新执行你这次做的事情
+                this.timer = setTimeout(()=>{
+                    const startY = this.startY
+                    // e.touches的第0项表示手指的一些信息
+                    // 求出的touchY是手指距离列表顶部的距离（即不包括上面的蓝色部分）
+                    const touchY = e.touches[0].clientY - 79
+                    const index = Math.floor((touchY-startY)/20)// 20是每个字母的高度
+                    if(index >=0 && index < this.letters.length){
+                        this.$emit('change',this.letters[index])
+                    }
+                },16)  
+            }
+        },
+        handleTouchEnd(){
+
+        }
     }
 }
 </script>
